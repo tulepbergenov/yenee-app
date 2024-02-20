cf266c64c28513ba90aa0e50301345ce57de1896
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-02-20 21:19:37 +0600

    Initial commit

---
+++ src/VApp.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VApp",
+});
+</script>
+
+<template>
+  <main>
+    <section>
+      <header>
+        <h1>Yenee</h1>
+      </header>
+    </section>
+  </main>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import VApp from "./VApp.vue";
+
+createApp(VApp).mount("#app");
