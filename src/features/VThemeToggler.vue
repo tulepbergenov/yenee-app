994cdc4cab9451db24d50b1b723d451b5b4bffc5
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-02-20 22:31:46 +0600

    style: update theme-toggler

--- src/features/VThemeToggler.vue
+++ src/features/VThemeToggler.vue
@@ -17,10 +17,42 @@ const onToggleDark = useToggle(isDark);
     :aria-label="isDark ? 'Switch to light theme' : 'Switch to dark theme'"
     :title="isDark ? 'Switch to light theme' : 'Switch to dark theme'"
     @click="onToggleDark()"
-    class="flex h-6 w-6 items-center justify-center"
+    class="relative flex h-6 w-6 items-center justify-center"
     type="button"
   >
-    <SunIcon v-if="isDark" />
-    <MoonIcon v-if="!isDark" />
+    <Transition name="sun">
+      <SunIcon
+        class="absolute left-2/4 top-2/4 h-full w-full -translate-x-2/4 -translate-y-2/4"
+        v-if="!isDark"
+      />
+    </Transition>
+    <Transition name="moon">
+      <MoonIcon
+        class="absolute left-2/4 top-2/4 h-full w-full -translate-x-2/4 -translate-y-2/4"
+        v-if="isDark"
+      />
+    </Transition>
   </button>
 </template>
+
+<style scoped>
+.sun-enter-active,
+.sun-leave-active {
+  @apply transition-[opacity,transform] duration-300 ease-in-out;
+}
+
+.sun-enter-from,
+.sun-leave-to {
+  @apply translate-x-2 opacity-0;
+}
+
+.moon-enter-active,
+.moon-leave-active {
+  @apply transition-[opacity,transform] duration-300 ease-in-out;
+}
+
+.moon-enter-from,
+.moon-leave-to {
+  @apply -translate-y-6 opacity-0;
+}
+</style>
